ccfb21471746ffd7fe1b53a55133acb63f17c843
Nikolay2005615 <kolasmykov20056@mail.ru>
2023-09-29 23:10:23 +0700

    correction 29.09.2023 3:10:22,98

--- Pushing_code.bat
+++ Pushing_code.bat
@@ -4,5 +4,5 @@ set timetemp=%time:~-10%
 
 git add .
 git commit -m "correction %datetemp% %timetemp%"
-git push https://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@github.com/Nikolay1562005/Hackathon_2023
+git push https://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@github.com/Nikolay1562005/Hackathon_2023
 pause
