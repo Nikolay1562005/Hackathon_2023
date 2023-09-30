db4fcd85d472f08cfaa716cb9f82b7d45ed82bc0
hixac <darkinbladechadshin92321@gmail.com>
2023-09-30 17:22:52 +0000

    FileField added!

--- bounty_hunter/main_menu/models.py
+++ bounty_hunter/main_menu/models.py
@@ -14,6 +14,7 @@ class BaseInfo(BaseModel):
 class User(BaseModel):
     first_name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50, null=True)
+    resumefile = models.FileField(null=False, blank=True)
     is_admin = models.BooleanField(default=False)
     info = models.ForeignKey(BaseInfo, null=True, on_delete=models.CASCADE)
     
